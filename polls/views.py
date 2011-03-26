ab25c9a77c9429b13a76f63c9f0e410544b8e46b
Neema Shakya <nshakya08@winona.edu>
2011-03-26 13:26:27 -0500

    html in .py

--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,15 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the poll index.")
+	
+# recall or note that %s means, "subsitute in a string"
+
+def detail(request, poll_id):
+    return HttpResponse("You're looking at poll <strong> %s. </strong>"  % ( poll_id,))
+
+def results(request, poll_id):
+    return HttpResponse("You're looking at the results of poll <strong> %s. </strong>" % (poll_id,))
+
+def vote(request, poll_id):
+    return HttpResponse("You're voting on poll <u> %s. </u>" % (poll_id,))
+   
