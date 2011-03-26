38da3dae3079d579f1aaaa019d95b1c04fbc0f4a
Neema Shakya <nshakya08@winona.edu>
2011-03-26 14:08:59 -0500

    Http response redirect

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 # Create your views here.
 
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 
 def index(request):
     return HttpResponse("Hello, world. You're at the poll index.")
@@ -16,3 +17,5 @@ def results(request, poll_id):
 def vote(request, poll_id):
     return HttpResponse("You're voting on poll <u> %s. </u>" % (poll_id,))
    
+def redirect_to_polls(request):
+    return HttpResponseRedirect('/polls/')
